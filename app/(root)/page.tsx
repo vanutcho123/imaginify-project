6eb65489ad25b5f0fc5a42a4ba9485890466971d
vanutcho123 <hainguyen.061001@gmail.com>
2024-03-20 14:33:25 +0700

    clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default HomePage;
